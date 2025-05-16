279432e786604d8266cef11beb774ffa319c2980
AKWIN07 <akwinjones1234@gmail.com>
2025-05-16 13:57:36 +0530

    Add files via upload

---
+++ KarkaTask/Trial/python/Task3/task3.py
@@ -0,0 +1,40 @@
+# def add(name):
+#     print(name)
+# add("hello world")
+
+# def add():
+#     a=10
+#     b=20
+#     sum=a+b
+#     print(sum)
+# add()
+
+# def multiply():
+#     a=20
+#     b=30
+#     print(a*b)
+# multiply()
+
+# def division():
+#     a=20
+#     b=10
+#     print(a/b)
+# division()
+
+# def factorial(num):
+#     fact=1
+#     if (num<=0):
+#         print('enter a number greater than one')
+#     elif (num==1):
+#         print("factorial of one is one")
+#     else:
+#         for i in range(1,num+1):
+#             fact*=i
+#             print(fact)
+# factorial(10)
+
+# def square_value():
+#     a=int(input("enter a number:"))
+#     b=a**2
+#     print("the square value is",b)
+# square_value()
---
+++ KarkaTask/Trial/python/Task4/task4.py
@@ -0,0 +1,79 @@
+# def pos_neg():
+#     n=int(input("enter a number"))
+#     if (n>0):
+#         print(n,"is a positive integer")
+#     else:
+#         print(n,"is negatyive integer")
+# pos_neg()
+
+# num=int(input("enter a number"))
+# if num%2==0:
+#     print('even number')
+# else:
+#     print('odd number')
+
+# a=int(input('enter a number:'))
+# b=int(input('enter a number:'))
+# c=a**b
+# print("square number 0f a nd b",c)
+
+# a=int(input('enter a number:'))
+# b=int(input('enter a number:'))
+# if (a>=b):
+#     print(a,"is greater than",b)
+# else:
+#     print(b,'is greater than',a)
+
+# a=int(input('enter a year:'))
+# if (a%4==0):
+#     print(a,"is an leap year")
+# else:
+#     print(a,'is not an leap year')
+
+# a=int(input('enter ur mark:'))
+# if (a>=400):
+#     print('u got A grade')
+# elif (a<=399 and a>=350):
+#     print('u got B grade')
+# elif (a<=349 and a>=300):
+#     print('u got C grade')
+# elif (a<=299 and a>=200):
+#     print('u got D grade')
+# else:
+#     print('u got F grade and u are failed')
+
+# age=int(input('enter ur age:'))
+# if (age<16):
+#     print("u can't drive")
+# elif(age==16 or age==17):
+#     print("you can drive but no vote")
+# elif(age>=18 and age<=24):
+#     print("u can vote but not rent a car")
+# else:
+#     print("you can do prtty much anything")
+
+
+
+# a=100
+# for i in range(1,a):
+#     if (i%3==0 and i%5==0):
+#         print("fizzbuzz")
+#     elif (i%3==0):
+#         print("fizz")
+#     elif (i%5==0):
+#         print("buzz")
+#     else:
+#         print(i)
+    
+
+
+# a=int(input('enter a year:'))
+# if (a%4==0 and a%100!=0) or (a%400==0):
+#     print(a,"is an leap year")
+# else:
+#     print(a,'is not an leap year')
+
+
+
+
+
